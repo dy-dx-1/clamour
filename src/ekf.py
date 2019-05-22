89ea0b2b6fbc24e0df6ec9ec6a936d74c56aa868
samarseneault <samuel.arseneault@polymtl.ca>
2019-05-22 09:10:53 -0400

    Added and slightly refactored ekf

---
+++ src/ekf.py
@@ -0,0 +1,122 @@
+from filterpy.kalman import ExtendedKalmanFilter
+from numpy import array, eye, asarray, dot, linalg
+
+class CustomEKF(ExtendedKalmanFilter):
+    def __init__(self):
+        super(CustomEKF, self).__init__(dim_x=9, dim_y=6)
+
+        self.dt = 0.1
+        self.setQF()
+        self.ps = []
+        self.zone_limits = [[0, 800, -100, 600]]
+        self.R = array([[15, 0, 0, 0, 0, 0],
+                        [0, 15, 0, 0, 0, 0],
+                        [0, 0, 15, 0, 0, 0],
+                        [0, 0, 0, 40, 0, 0],
+                        [0, 0, 0, 0, 40, 0],
+                        [0, 0, 0, 0, 0, 40]])
+
+        self.RRange = array([[2, 0, 0, 0, 0, 0],
+                             [0, 2, 0, 0, 0, 0],
+                             [0, 0, 2, 0, 0, 0],
+                             [0, 0, 0, 40, 0, 0],
+                             [0, 0, 0, 0, 40, 0],
+                             [0, 0, 0, 0, 0, 40]])
+
+        # compute Jacobian of H matrix
+        self.H_of_pose = array([[1, 0, 0, 0, 0, 0, 0 ,0 ,0],
+                                [0, 0, 0, 1, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+
+    def setQF(self):
+        self.Q = array([[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, self.dt/10, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, self.dt, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, self.dt/10, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, self.dt, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, self.dt/10, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, self.dt]])
+
+        self.F = eye(9) + array([[0, self.dt, self.dt*self.dt, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, self.dt, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, self.dt, self.dt*self.dt, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, self.dt, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, self.dt, self.dt*self.dt],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, self.dt],
+                                 [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+    def hx_of_pose(self, x):
+        """ takes a state variable and returns the measurements that would
+        correspond to that state."""
+        return dot(self.H_of_pose(x), x)
+
+    def H_of_range(self, x, nei_pose):
+        """ compute Jacobian of H matrix for state x """
+        numNei = nei_pose.shape
+        deltas = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        for i in range(3):
+            if numNei[0] > i:
+                norm = linalg.norm([x[0]-nei_pose[i][0], x[3]-nei_pose[i][1], x[6]-nei_pose[i][2]])
+                for j in range(3):
+                    deltas[j] = (x[j*3]-nei_pose[i][j])/norm
+
+        return array([[deltas[0], 0, 0, deltas[1], 0, 0, deltas[2] ,0 ,0],
+                      [deltas[3], 0, 0, deltas[4], 0, 0, deltas[5] ,0 ,0],
+                      [deltas[6], 0, 0, deltas[7], 0, 0, deltas[8] ,0 ,0],
+                      [0, 0, 1, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+    def hx_of_range(self, x, nei_pose):
+        """ takes a state variable and returns the measurements that would
+        correspond to that state."""
+        nb_neighbors = nei_pose.shape[0]
+        hx_out = [0, 0, 0, x[2], x[5], x[8]]
+
+        for i in range(3):
+            if nb_neighbors[0] > i:
+                hx_out[i] = linalg.norm([x[0]-nei_pose[i][0], x[3]-nei_pose[i][1], x[6]-nei_pose[i][2]])
+
+        return hx_out
+
+    def setPose(self, position):
+        self.x = array([position.x/10, 0, 0, position.y/10, 0, 0, position.z/10, 0, 0])
+
+    def pre_update(self, dt):
+        if(dt != self.dt):
+            self.dt = dt
+            self.setQF()
+        self.predict()
+
+    def updatePose(self, position, acceleration, dt):
+        self.pre_update(dt)
+        super(CustomEKF, self).update(asarray([position[0], position[1], position[2], acceleration.x/10, acceleration.y/10, acceleration.z/10]), 
+                                      self.H_of_pose, self.hx_of_pose, self.R)
+
+    def updateRange(self, new_range, nei_pose, acceleration, dt):
+        self.pre_update(dt)
+        super(CustomEKF, self).update(asarray([new_range[0], new_range[1], new_range[2], acceleration.x/10, acceleration.y/10, acceleration.z/10]), 
+                                      self.H_of_range, self.hx_of_range, self.RRange, args=nei_pose, hx_args=nei_pose)
+
+    def check_zone(self, zone):
+        if self.x[0] < self.zone_limits[zone][0]:
+            self.x[0] = self.zone_limits[zone][0]
+
+        if self.x[0] > self.zone_limits[zone][1]:
+            self.x[0] = self.zone_limits[zone][1]
+
+        if self.x[3] < self.zone_limits[zone][2]:
+            self.x[3] = self.zone_limits[zone][2]
+
+        if self.x[3] > self.zone_limits[zone][3]:
+            self.x[3] = self.zone_limits[zone][3]
+
