b6d4c73b1eca22f9e37175253cb1fe55d80e582f
samarseneault <samuel.arseneault@polymtl.ca>
2019-05-24 14:48:32 -0400

    Scheduling state now uses Messenger

--- src/states/scheduling.py
+++ src/states/scheduling.py
@@ -1,27 +1,28 @@
 from .constants import State
 from .tdmaState import TDMAState
 
+from messenger import Messenger
 from interfaces import Neighborhood, SlotAssignment, Anchors, Timing
 from interfaces.timing import syncTimeLen, numNodes, tdmaSchSlotLen, tdmaExcStartTime
 from pypozyx import PozyxSerial
 
 class Scheduling(TDMAState):
     def __init__(self, neighborhood: Neighborhood, slot_assignment: SlotAssignment,
-                 timing: Timing, anchors: Anchors, id: int):
+                 timing: Timing, id: int, messenger: Messenger):
         self.neighborhood = neighborhood
         self.slot_assignment = slot_assignment
-        self.anchors = anchors
         self.timing = timing
         self.id = id
+        self.messenger = messenger
 
     def execute(self):
         self.neighborhood.neighbor_list = self.neighborhood.synchronized_neighbors
         self.slot_assignment.update_free_slots()
 
         if int(((self.timing.current_time_in_cycle - syncTimeLen) % (numNodes*tdmaSchSlotLen)) / tdmaSchSlotLen) == self.id - 99:
-            message_handler.broadcast_control_message()
+            self.messenger.broadcast_control_message()
         else:
-            message_handler.receive_message()
+            self.messenger.receive_message()
         
         self.slot_assignment.update_free_slots()
         self.update_pure_send_list()
