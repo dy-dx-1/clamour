1ffceb2af89bb8104327c3dc6ad9cb50a2d51576
samarseneault <samuel.arseneault@polymtl.ca>
2019-06-11 13:21:30 -0400

    corrected some imports

--- src/clamour.py
+++ src/clamour.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
-from .tdmaNode import TDMANode
+from tdmaNode import TDMANode
 
 
 def main():
--- src/interfaces/timing.py
+++ src/interfaces/timing.py
@@ -1,4 +1,8 @@
-from ..logicalClock import LogicalClock
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from logicalClock import LogicalClock
 
 COMMUNICATION_DELAY = 0.027  # delay estimation is 0.005s.
 SLOT_FOR_RESET = 30
