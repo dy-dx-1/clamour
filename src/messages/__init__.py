9dd9aebff29b675fb870094057cd50ec8a54e2e6
samarseneault <samuel.arseneault@polymtl.ca>
2019-06-18 10:56:37 -0400

    Added exception class to __init__.py

--- src/messages/__init__.py
+++ src/messages/__init__.py
@@ -1,5 +1,5 @@
 from .messageBox import MessageBox
-from .messageFactory import MessageFactory
+from .messageFactory import MessageFactory, InvalidMessageTypeException
 from .synchronizationMessage import SynchronizationMessage
 from .types import MessageType
 from .uwbMessage import (UWBCommunicationMessage, UWBMessage, UWBSynchronizationMessage, UWBTDMAMessage)
