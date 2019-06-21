8bb3bc888a7a167b6c6fde4c3e2d555bb27af625
samarseneault <samuel.arseneault@polymtl.ca>
2019-06-21 15:36:30 -0400

    Added measured anchors to anchors_list

--- src/interfaces/anchors.py
+++ src/interfaces/anchors.py
@@ -1,5 +1,11 @@
+from pypozyx import DeviceCoordinates, Coordinates
+
+
 class Anchors:
     def __init__(self):
         self.available_anchors = []
-        self.anchors_list = []  # Might need to be hardcoded later based on anchor positions in the museum.
-        self.anchors_dict = {self.anchors_list[i].data[0]: self.anchors_list[i] for i in self.anchors_list}
+        self.anchors_list = [DeviceCoordinates(0x6e27, 1, Coordinates(1700, 5170, 1840)),
+                             DeviceCoordinates(0x6e4f, 1, Coordinates(0, 0, 1330)),
+                             DeviceCoordinates(0x6e42, 1, Coordinates(-1780, 4480, 2230))]
+
+        self.anchors_dict = {anchor.data[0]: anchor for (_, anchor) in enumerate(self.anchors_list)}
