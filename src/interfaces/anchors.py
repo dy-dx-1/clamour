86583f9c85ce17286fcbab052f0a2b228c32387a
samarseneault <samuel.arseneault@polymtl.ca>
2019-07-16 14:22:27 -0400

    Added 4th anchor

--- src/interfaces/anchors.py
+++ src/interfaces/anchors.py
@@ -6,6 +6,7 @@ class Anchors:
         self.available_anchors = []
         self.anchors_list = [DeviceCoordinates(0x100B, 1, Coordinates(1700, 5170, 1840)),
                              DeviceCoordinates(0x100A, 1, Coordinates(0, 0, 1330)),
-                             DeviceCoordinates(0x100C, 1, Coordinates(-1780, 4480, 2230))]
+                             DeviceCoordinates(0x100C, 1, Coordinates(-1780, 4480, 2230)),
+                             DeviceCoordinates(0x1006, 2, Coordinates(3964, 1989, 764))]
 
         self.anchors_dict = {anchor.data[0]: anchor for (_, anchor) in enumerate(self.anchors_list)}
