ffb4b8dfcaa166994f5eaf4a320eae7509b4b2a6
samarseneault <samuel.arseneault@polymtl.ca>
2019-07-30 10:52:30 -0400

    Room orientation is now based on corner coordinates

---
+++ src/rooms/roomLoader.py
@@ -0,0 +1,31 @@
+import csv
+import math
+from .room import Room
+
+
+class RoomLoader:
+    @staticmethod
+    def load_rooms_from_csv() -> list:
+        with open('rooms/rooms.csv') as r:
+            reader = csv.reader(r, delimiter=';')
+            next(reader)  # We don't want to read the header, so we skip it
+            return [RoomLoader.create_room(room) for room in reader]
+
+    @staticmethod
+    def create_room(room_data: list) -> Room:
+        label = room_data[0]
+        x = int(room_data[5])
+        y = int(room_data[6])
+        x_lim = (int(room_data[1]) / 2, int(room_data[1]) / 2)
+        y_lim = (int(room_data[2]) / 2, int(room_data[2]) / 2)
+        theta = RoomLoader.orientation_from_corners(room_data[7:])
+
+        return Room(label, x, y, x_lim, y_lim, theta)
+
+    @staticmethod
+    def orientation_from_corners(corners: list) -> float:
+        diff_x = int(corners[2]) - int(corners[4])
+        diff_y = abs(int(corners[3]) - int(corners[5]))
+        print(diff_x, diff_y)
+
+        return math.atan2(diff_y, diff_x)
