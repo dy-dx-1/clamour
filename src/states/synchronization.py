00fd0a8eb84b815c0c7228918197b66aaf537bc6
samarseneault <samuel.arseneault@polymtl.ca>
2019-08-23 16:20:10 -0400

    Added logging for synced neighbors

--- src/states/synchronization.py
+++ src/states/synchronization.py
@@ -42,6 +42,8 @@ class Synchronization(TDMAState):
         return next_state
 
     def next(self) -> State:
+        if len(self.neighborhood.synced_neighbors) > 0:
+            print("Synced neibs: ", self.neighborhood.synced_neighbors)
         if self.neighborhood.is_alone() or \
                 ((self.timing.current_time_in_cycle > SYNCHRONIZATION_PERIOD and self.timing.synchronized) and
                     self.neighborhood.are_neighbors_synced()):  # TODO: make sure it doesnt get stuck forever
