7bc074bedd6b2f54c2dde83f6d9cadabdadfd30c
samarseneault <samuel.arseneault@polymtl.ca>
2019-09-10 15:40:41 -0400

    Scaling factor

--- src/logicalClock.py
+++ src/logicalClock.py
@@ -9,7 +9,7 @@ class LogicalClock:
         self.clock = 0
 
     def update_clock(self) -> None:
-        self.clock += (perf_counter() - self.last_hardware_time)
+        self.clock += (perf_counter() - self.last_hardware_time) * 1000
         print(f"Logical clock: {self.clock}")
         self.last_hardware_time = perf_counter()
 
