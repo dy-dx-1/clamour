007d141c8ea56cc190590a2589a0fadbd5245d16
samarseneault <samuel.arseneault@polymtl.ca>
2019-09-10 15:42:53 -0400

    Removed log

--- src/logicalClock.py
+++ src/logicalClock.py
@@ -10,7 +10,7 @@ class LogicalClock:
 
     def update_clock(self) -> None:
         self.clock += (perf_counter() - self.last_hardware_time) * 1000
-        print(f"Logical clock: {self.clock}")
+        # print(f"Logical clock: {self.clock}")
         self.last_hardware_time = perf_counter()
 
     def correct_logical_offset(self, correction: int) -> None:
