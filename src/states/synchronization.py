ff7e7153c4e62d2f789db3ba6c0c104e8c084abc
David <david.st-onge@etsmtl.ca>
2019-09-18 17:21:24 -0400

    Removed timer from sync condition

--- src/states/synchronization.py
+++ src/states/synchronization.py
@@ -72,7 +72,7 @@ class Synchronization(TDMAState):
         if self.neighborhood.is_alone_in_state(State.SYNCHRONIZATION) or \
                 current_exec_time > SYNCHRONIZATION_PERIOD or \
                 ((self.timing.synchronized or self.is_left_behind()) and
-                 (self.neighborhood.are_neighbors_synced() and current_exec_time > SYNCHRONIZATION_PERIOD / 3)):
+                 self.neighborhood.are_neighbors_synced()):
             print("Going to scheduling from sync")
             return State.SCHEDULING
         else:
