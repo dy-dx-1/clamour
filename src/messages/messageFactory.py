161aac1896582511683f16d32b389a1068be46b9
samarseneault <samuel.arseneault@polymtl.ca>
2019-09-19 09:20:03 -0400

    Added masks and bitshifts to process custom messages

--- src/messages/messageFactory.py
+++ src/messages/messageFactory.py
@@ -2,20 +2,31 @@ from .types import MessageType
 from .uwbMessage import (UWBMessage, UWBSynchronizationMessage, UWBTDMAMessage)
 
 
+CUSTOM_MESSAGE_SIGNATURE = 0xAA
+CUSTOM_MESSAGE_MASK = 0xFF00000000
+CUSTOM_MESSAGE_MASK_INV = 0x00FFFFFFFF
+TYPE_BIT_MASK = 0x80000000
+
+
 class InvalidMessageTypeException(Exception):
     pass
 
 
 class MessageFactory:
     @staticmethod
-    def create(sender_id: int, message_data: int=0) -> UWBMessage:
-        type_bit_mask = 0x80000000
-        message_type = (message_data & type_bit_mask) >> 31
+    def create(sender_id: int, raw_data: int = 0) -> UWBMessage:
+        message_type = (raw_data & TYPE_BIT_MASK) >> 31
+        message_data = raw_data & CUSTOM_MESSAGE_MASK_INV
 
-        if message_type == MessageType.SYNC:
+        if MessageFactory.is_custom_message(raw_data) and message_type == MessageType.SYNC:
             return UWBSynchronizationMessage(sender_id, message_type, message_data)
-        elif message_type == MessageType.TDMA:
+        elif MessageFactory.is_custom_message(raw_data) and message_type == MessageType.TDMA:
             # TODO: Add two-hop neighbor message
             return UWBTDMAMessage(sender_id, message_type, message_data)
         else:
             raise InvalidMessageTypeException("The message type in the received data does not match any known message type.")
+
+    @staticmethod
+    def is_custom_message(data: int) -> bool:
+        print(hex((data & CUSTOM_MESSAGE_MASK) >> 32))
+        return (data & CUSTOM_MESSAGE_MASK) >> 32 == CUSTOM_MESSAGE_SIGNATURE
