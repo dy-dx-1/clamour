32cf3bf38644c1b8ac73c3d71546cf15c23a982f
David <david.st-onge@etsmtl.ca>
2019-09-19 16:08:08 -0400

    fix scaling bug

--- src/rooms/roomLoader.py
+++ src/rooms/roomLoader.py
@@ -14,10 +14,10 @@ class RoomLoader:
     @staticmethod
     def create_room(room_data: list) -> Room:
         label = room_data[0]
-        x = int(room_data[5]*10)
-        y = int(room_data[6]*10)
-        x_lim = (int(room_data[3]*10), int(room_data[3]*10))
-        y_lim = (int(room_data[4]*10), int(room_data[4]*10))
+        x = int(room_data[5])*10
+        y = int(room_data[6])*10
+        x_lim = (int(room_data[3])*10, int(room_data[3])*10)
+        y_lim = (int(room_data[4])*10, int(room_data[4])*10)
         theta = RoomLoader.orientation_from_corners(room_data[7:])
 
         return Room(label, x, y, x_lim, y_lim, theta)
