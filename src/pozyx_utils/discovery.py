970d730e7649580d795835ccf9c1377840720845
David <david.st-onge@etsmtl.ca>
2019-09-20 03:40:56 -0400

    fix device type

--- src/pozyx_utils/discovery.py
+++ src/pozyx_utils/discovery.py
@@ -41,9 +41,9 @@ class PozyxDiscoverer:
             PozyxDiscoverer.handle_error(pozyx, pozyx_lock, "get_device_list")
 
         if discovery_type == POZYX_DISCOVERY_TAGS_ONLY:
-            devices = [device for device in devices if not PozyxDiscoverer.is_anchor(device)]
+            devices = [device_id for device_id in devices if not PozyxDiscoverer.is_anchor(device_id)]
         elif discovery_type == POZYX_DISCOVERY_ANCHORS_ONLY:
-            devices = [device for device in devices if PozyxDiscoverer.is_anchor(device)]
+            devices = [device_id for device_id in devices if PozyxDiscoverer.is_anchor(device_id)]
 
         return devices
 
