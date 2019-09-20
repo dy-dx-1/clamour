8c89c4b6820984d13ba79676c8aaea9a0106b94b
David <david.st-onge@etsmtl.ca>
2019-09-20 05:39:11 -0400

    add sd floor height

--- src/interfaces/anchors.py
+++ src/interfaces/anchors.py
@@ -7,6 +7,7 @@ class Anchors:
         self.available_anchors = []
         self.available_tags = []
         self.anchors_list = self.load_anchors_from_csv()
+        self.floor_height = 13800
 
         print("Anchor list loaded (", len(self.anchors_list), ")")
 
@@ -21,10 +22,12 @@ class Anchors:
     @staticmethod
     def add_anchor(anchor_data: list) -> DeviceCoordinates:
         label = int(anchor_data[0], base=16)
-        x = int(anchor_data[1])
-        y = int(anchor_data[2])
-        z = int(anchor_data[3])
+        lvl = int(anchor_data[1])
+        x = int(anchor_data[2])
+        y = int(anchor_data[3])
+        z = int(anchor_data[4])
 
-        print(label)
+        if lvl == 2:
+            z += self.floor_height
 
         return DeviceCoordinates(label, 1, Coordinates(x, y, z))
