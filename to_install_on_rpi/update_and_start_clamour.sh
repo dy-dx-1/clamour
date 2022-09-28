007d14cc8726d91454d80684047e0a2a68c035d9
Thinking-Dragon <cfmgp@hotmail.com>
2022-09-28 13:07:44 -0400

    Add service and script to update and start clamour on edge device

---
+++ to_install_on_rpi/update_and_start_clamour.sh
@@ -0,0 +1,3 @@
+# Replace address_to_your_docker_registry to fit your registry
+sudo docker pull address_to_your_docker_registry/clamour
+sudo docker run --device /dev/ttyACM0 address_to_your_docker_registry/clamour
