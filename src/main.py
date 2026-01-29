f72a6cfafb982bb81e6178bab9cebeafa7eca921
Nicolas Sarabia-Benoit <nic19sarbe@gmail.com>
2026-01-28 19:51:54 -0500

    created .gitignore + minor reformatting

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import sys
 import time
 
 def onNewPoseEstimated(poseMsg: PoseMessage):
+    ### PoseMessage is defined in ./clamour/messages/poseMessage.py and it's attributes describe the pose of an agent
     print("New pose estimated: x:", poseMsg.x, ", y:", poseMsg.y, ", z:", poseMsg.z, ", yaw:", poseMsg.yaw)
 
 if __name__ == "__main__":
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         sound = bool(int(sys.argv[1]))
         
-    communication_queue = ContextManagedQueue()
+    communication_queue = ContextManagedQueue() # ContextManagedQueue is an extension of a multiprocessing.Queue() with a max size of 20
 
     clamour = Clamour([])
     clamour.start(sound, onNewPoseEstimated, communication_queue)
