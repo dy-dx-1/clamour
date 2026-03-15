93bb70ec6e688bbfd3312ab272909c0b18a132c2
dy-dx-1 <nic19sarbe@gmail.com>
2026-03-15 10:56:51 -0400

    init new objects containers file

---
+++ src/clamour/interfaces/containers.py
@@ -0,0 +1,7 @@
+""" 
+This file defines many objects that serve to transport info from the tag to Clamour. 
+Originally, these were directly used from the pypozyx library. In an effort to decouple the code from it, 
+these are general versions that can be used in a platform agnostic manner. 
+"""
+class Coordinates: 
+    pass 
