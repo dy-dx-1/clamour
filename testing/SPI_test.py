f686276f3a92f5efd5cb363ffee36304e6a2deab
dy-dx-1 <nic19sarbe@gmail.com>
2026-06-01 15:08:24 -0400

    added default arg to reverse

--- testing/SPI_test.py
+++ testing/SPI_test.py
@@ -6,13 +6,9 @@ parent_dir = str(Path(__file__).resolve().parent.parent)
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
-import RPi.GPIO
-import spidev 
-import time 
-
 from src.clamour.interfaces.dw_1000 import DW1000 
 
 with DW1000(bus=0, cs=0) as dw:
-    print("init") 
+    eui = dw.read_register(0x01, 8)
+    print(eui) 
 
-print("done") 
