7d512c2f000a40c8910ca0dd660ccc73bfa17ce2
dy-dx-1 <nic19sarbe@gmail.com>
2026-07-01 10:49:02 -0400

    wrapped in main()

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from clamour.custom_terminal import print
 def onNewPoseEstimated(poseMsg: PoseMessage):
     print(text=f"Pose estimated: x: {poseMsg.x}, y: {poseMsg.y}, z: {poseMsg.z}, yaw: {poseMsg.yaw}", status='info', type='loc')
 
-if __name__ == "__main__":
-    # An argument of anything else than 0 sets debug to True.
+def main(): 
+# An argument of anything else than 0 sets debug to True.
     sound = False
     if len(sys.argv) > 1:
         sound = bool(int(sys.argv[1]))
@@ -17,3 +17,6 @@ if __name__ == "__main__":
 
     clamour = Clamour([])
     clamour.start(sound, onNewPoseEstimated, communication_queue)
+
+if __name__ == "__main__":
+    main() 
