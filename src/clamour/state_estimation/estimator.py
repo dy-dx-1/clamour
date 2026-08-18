280f885b4f3359bd8cc23a384b4a81207b1ae3ad
dy-dx-1 <nic19sarbe@gmail.com>
2026-08-18 11:06:55 -0400

    decoupled trilateration from initialization. simplifying csv saving. new updatemessage ready for testing

--- src/clamour/state_estimation/estimator.py
+++ src/clamour/state_estimation/estimator.py
@@ -1,7 +1,7 @@
 import os 
 import csv
 import math 
-import numpy.linalg as linalg
+import numpy as np 
 from time import sleep, time 
 from typing import Literal
 from struct import error as StructError
@@ -41,7 +41,7 @@ class StateEstimator:
         self.tag_lock = tag_lock 
 
         self.estimator = None 
-        self.estimator_type = estimator_type # validity check done in config TODO 
+        self.estimator_type = estimator_type # validity check done in config  
 
         self.yaw_offset = 0  # Measured in degrees relative to global coordinates X-Axis
         self.last_know_neighbors = {}
@@ -73,23 +73,25 @@ class StateEstimator:
             if not self.com_queue.empty():
                 msg = UpdateMessage.load(*self.com_queue.get_nowait())
                 if msg.update_type == UpdateType.RANGING: 
-                    if not len(msg.anchors_ranging_data )<3:  # Need a fully constrained measurement for initialization
+                    if not len(msg.anchors_ranging_data)<3:  # Need a fully constrained measurement for initialization
                         continue 
-                    self.yaw_offset = msg.measured_yaw  # Store initial value, which we'll use to correct further poses 
-                    # TODO pick back up here, add a function that does simple trilat or mean of anchors for proper start position 
-                    raw_pos, raw_yaw = msg.measured_xyz, self.correct_yaw(msg.measured_yaw)
+                    self.yaw_offset = msg.measured_yaw  # Store initial value, which we'll use to correct further poses  
+                    # Whatever the estimator, initializing the position with trilateration or it's equivalent is done through
+                    # non-linear optimization, which requires an initial guess to converge. We set this initial value
+                    # to the simple centroid of the available anchors 
+                    initial_pos = self.calculate_3D_mean([data[0].data for data in msg.anchors_ranging_data])
+                    raw_yaw = self.correct_yaw(msg.measured_yaw)
                     
                     if self.estimator_type == 'EKF': 
-                        self.estimator = CustomEKF(raw_pos, raw_yaw)
-                        self.estimator.trilateration_update(raw_pos, raw_yaw, msg.timestamp)
+                        self.estimator = CustomEKF(initial_pos, raw_yaw)
+                        # For the EKF, incorporating ranging data with >3 anchors will directly trigger a trilateration update
+                        self.estimator.incorporate_ranging_data(msg.timestamp, msg.anchors_ranging_data, msg.tags_ranging_data, raw_yaw)
                     elif self.estimator_type == 'FG':
                         # TODO 
                         pass 
 
-                    # Estimator initialized. Get posterior on position and yaw, save and print output 
-                    post_pos, post_yaw = self.estimator.get_position(), self.estimator.get_yaw() 
-                    self.save_to_csv(msg.timestamp, msg, post_pos, post_yaw)
-                    self.pose_callback(PoseMessage(post_pos.x, post_pos.y, post_pos.z, post_yaw))
+                    # Estimator initialized. Internalise and publish the posterior.  
+                    self.publish_state(msg) 
             else:
                 sleep(WAIT_TIME_DURING_INIT)  
         print(f"ESTIMATOR ({self.estimator_type}) INITIALIZATION DONE", 'ok', 'loc')
@@ -101,31 +103,20 @@ class StateEstimator:
         """
         if not self.com_queue.empty(): 
             msg = UpdateMessage.load(*self.com_queue.get_nowait())
-            if msg.update_type != UpdateType.TOPOLOGY: # TODO ensure compatible with pedometer/custom update
-                anchors_ranging_data, tags_ranging_data, raw_yaw, timestamp = msg.anchors_ranging_data, msg.tags_ranging_data, msg.measured_yaw, msg.timestamp
+            ts, anchors_ranging, tags_ranging, raw_yaw = msg.timestamp, msg.anchors_ranging_data, msg.tags_ranging_data, msg.measured_yaw
             # TODO add an in-bounds of the room check somewhere 
             match msg.update_type: 
-                # NOTE TODO, figure out how to deal with trilateration vs taking all measuremetns for FG. Abstract trilat choice into the ekf and just pass all ranges to FG?
-                # or keep trilat as a general message for passing enough ranges? 
                 case UpdateType.PEDOMETER: 
-                    self.estimator.pedometer_update(self.pedometer_yaw_to_coords(raw_yaw), raw_yaw, timestamp) 
+                    self.estimator.pedometer_update(self.pedometer_yaw_to_coords(msg.measured_yaw), raw_yaw, ts) 
                 case UpdateType.RANGING: 
                     self.update_neighbors(msg.topology) 
-                    self.estimator.incorporate_ranging_data(timestamp, anchors_ranging_data, tags_ranging_data, raw_yaw) 
+                    self.estimator.incorporate_ranging_data(ts, anchors_ranging, tags_ranging, raw_yaw) 
                 case UpdateType.TOPOLOGY:  
                     self.update_neighbors(msg.topology) 
-                    raw_pos, raw_yaw = self.estimator.get_position(), self.estimator.get_yaw() # just to have something to print for callback 
                 case UpdateType.CUSTOM_POSE: # TODO remove / replace by IMU factor? 
-                    self.estimator.custom_odometry_update(Coordinates(msg.pose.x, msg.pose.y, msg.pose.z), msg.pose.yaw, msg.R, timestamp) 
-            try: 
-                with self.tag_lock:
-                    self.tag.coordinates = self.estimator.get_position() 
-            except StructError as s: 
-                print(f"Estimator couldn't get new state estimate in process_latest_state_info(): {str(s)}", 'error', 'loc')
-
-            # TODO fix these, raw_pos is no longer relevant as estimation is abstracted by ranges
-            self.save_to_csv(self.estimator.last_measurement_time, msg, raw_pos, raw_yaw) 
-            self.pose_callback(PoseMessage(raw_pos.x, raw_pos.y, raw_pos.z, raw_yaw))
+                    self.estimator.custom_odometry_update(Coordinates(msg.pose.x, msg.pose.y, msg.pose.z), msg.pose.yaw, msg.R, msg.timestamp) 
+            
+            self.publish_state(msg) 
 
             if self.sound_queue != None: 
                 sound_message = SoundMessage(self.estimator.get_position())
@@ -139,6 +130,24 @@ class StateEstimator:
                                       self.estimator.last_measurement_time + ZERO_MVT_THRESHOLD) # need to abstract threshold, may not needed for FG? Maybe dont need zero mvt update at all?
         else: 
             sleep(WAIT_TIME_DURING_INIT) 
+
+    def calculate_3D_mean(points:list[tuple])->Coordinates:
+        """
+        Takes a list of at least 3 positions and calculates the mean. 
+        This is used in initialize_estimator as a first estimate of the tag's position. 
+        - points: [(x1,y1,z1), (x2,y2,z2), (x3,y3,z3), ...] 
+        """ 
+        points_array = np.array(points)
+        
+        # Validation checks
+        if points_array.ndim != 2 or points_array.shape[1] != 3:
+            raise ValueError("Each point in the input array must have exactly 3 coordinates (X, Y, Z).")
+        if points_array.shape[0] < 3:
+            raise ValueError("Input must contain at least 3 distinct 3D positions.")
+            
+        # Compute the average along the row axis
+        mean = np.mean(points_array, axis=0) 
+        return Coordinates(int(mean[0]), int(mean[1]), int(mean[2]))
 
     def update_neighbors(self, neighbors: dict):
         self.last_know_neighbors = neighbors
@@ -181,6 +190,21 @@ class StateEstimator:
 
         return False
 
+    def publish_state(self, message: UpdateMessage): 
+        """
+        - Saves the position in it's Tag object 
+        - Prints out the current posterior from the estimator
+        - Saves to CSV if configured to do so (config.py) 
+        """
+        with self.tag_lock:
+            self.tag.coordinates = self.estimator.get_position() 
+
+        post_pos, post_yaw = self.estimator.get_position(), self.estimator.get_yaw() 
+        self.pose_callback(PoseMessage(post_pos.x, post_pos.y, post_pos.z, post_yaw))
+
+        if SAVE_TO_CSV: 
+            self.save_to_csv(message)
+
     @staticmethod 
     def initialize_csv(): 
         if not SAVE_TO_CSV: 
@@ -188,7 +212,7 @@ class StateEstimator:
         filepath = 'pose_estimation.csv'
         is_new_file = os.path.exists(filepath)
         fieldnames = ['tag_id', 'timestamp', 'synchronized_clock', 'offset', 'update_type',
-                      'raw_x', 'estimator_x', 'raw_y', 'estimator_y', 'raw_z', 'estimator_z', 'raw_yaw', 'estimator_yaw', 
+                      'estimator_x', 'estimator_y', 'estimator_z', 'estimator_yaw', 
                       'covariance_matrix', 'slots', 'two_hop_neighbors']
 
         state_csv = open(filepath, 'w')
@@ -198,25 +222,29 @@ class StateEstimator:
 
         return state_csv, writer
     
-    def save_to_csv(self, timestamp: float, message: UpdateMessage, coordinates: Coordinates, yaw: float) -> None:
-        if not SAVE_TO_CSV: 
-            return 
-        if coordinates is not None and message.update_type != UpdateType.CUSTOM_POSE:
+    def save_to_csv(self, message: UpdateMessage):
+        """
+        NOTE: Previously, this function also printed the raw position pre-EKF. During the move to FGs, I've removed this option. 
+        This is because we now directly deal with multiple ranges, so we'd either need to:
+        - Add a function inside the estimator that returns the 'non optimized' geometric value 
+        - Add a function on here that quickly computes an estimation to generate raw values (not ideal, as we'd be doing the calc twice)
+        ALSO: would need to figure out how to consider the IMU effect on the 'raw pos'. Do we incorporate it? Or do we just print out geometric results?
+        What would be the benefit?
+
+        Since the answer to these questions may be clearer in the future, leaving it without raw position for now (2026-08-18). 
+        """
+        if message.update_type == UpdateType.RANGING: # TODO add compatibility to IMU in future 
             csv_data = {
                 'tag_id': self.tag.tag_id,
-                'timestamp': timestamp,
+                'timestamp': message.timestamp,
                 'synchronized_clock': message.synchronized_clock,
                 'offset': message.offset,
                 'update_type': message.update_type,
-                'raw_x': coordinates.x,
                 'estimator_x': self.estimator.get_position().x,
-                'raw_y': coordinates.y,
                 'estimator_y': self.estimator.get_position().y,
-                'raw_z': coordinates.z,
                 'estimator_z': self.estimator.get_position().z,
-                'raw_yaw': yaw,
                 'estimator_yaw': self.estimator.get_yaw(),
-                'covariance_matrix': linalg.det(self.estimator.P),
+                'covariance_matrix': np.linalg.det(self.estimator.P),
                 'slots': message.slots,
                 'two_hop_neighbors': self.last_know_neighbors
             }
