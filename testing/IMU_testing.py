6a66bdd94e06715249037f1245e4078e27b5da03
dy-dx-1 <nic19sarbe@gmail.com>
2026-09-01 12:21:53 -0400

    pitch roll yaw rate extraction

--- testing/IMU_testing.py
+++ testing/IMU_testing.py
@@ -1,5 +1,12 @@
 import smbus2
 
+def unsigned_to_signed(value:int)->int: 
+    """
+    Converts an unsigned 2-byte int to a signed int, following two's complement 
+    """ 
+    return value - 0x10000 if value & 0x8000 else value
+
+
 class LSM6DSV320X: 
     def __init__(self, SDO_state:bool, i2c_bus:int=1): 
         """
@@ -33,13 +40,15 @@ class LSM6DSV320X:
         except: 
             pass
 
-    def read_reg(self, reg_address:int)->int: 
+    # TODO not sure I need these tbh 
+    def read_reg_byte(self, reg_address:int)->int: 
         """Helper to read a single byte from a register address without having to rewrite the target address every time"""
         return self.bus.read_byte_data(self.TAD, reg_address)
 
-    def write_reg(self, reg_address:int, byte:int): 
+    def write_reg_byte(self, reg_address:int, byte:int): 
         """Helper to write a single byte to a register address without having to rewrite the target address every time"""
         self.bus.write_byte_data(self.TAD, reg_address, byte)
+
 
     def validate_connection(self): 
         """
@@ -53,7 +62,7 @@ class LSM6DSV320X:
 
     def configure(self): 
         """
-        Configures the settings of the IMU. 
+        Configures the settings of the IMU and the internal dependent conversion factors.  
         
         **Currently only supporting**:
         - High performance mode accel/gyro
@@ -68,46 +77,56 @@ class LSM6DSV320X:
         - CTRL8 (0x17) 
         """
         ### Some configs need to be done with the accelerometer and gyro in power down mode, so we first turn them off 
-        self.write_reg(0x10, 0x00)
-        self.write_reg(0x11, 0x00) 
+        self.write_reg_byte(0x10, 0x00)
+        self.write_reg_byte(0x11, 0x00) 
         ### CTRL6 (0x15) Gyro bandwidth 
         lpf1_bw = 0b0000 # NOTE bandwidth selection currently hardcoded at 120Hz 
         fs_g = 0b1010    # NOTE Currently hardcoded at 500 dps, see p.69 of manual for others 
-        self.write_reg(0x15, lpf1_bw<<4|fs_g)
+        self.LSB_TO_MDPS = 17.5 # CONVERSION FACTOR FOR 500DPS p.12
+        self.write_reg_byte(0x15, lpf1_bw<<4|fs_g)
         ### CTRL8 (0x17) Accelerometer scale 
         # not touching HP_LPF2_XL_BW_2 
         fs_xl = 0b00 # NOTE currently hardcoded +-2g (see p.71 to change) 
-        self.write_reg(0x17, fs_xl) 
+        self.write_reg_byte(0x17, fs_xl) 
         ### Accelerometer control reg 1 - CTRL1 - 0x10 
         # AND 
         ### Gyroscope control reg 2 - CTRL2 - 0x11 
         # The 4 MSBs will all be 0 as long as we only support high-perf mode
         # NOTE For now hardcoding 120Hz, see p.65 of manual if want other ones 
-        self.write_reg(0x10, 0b00000110)
-        self.write_reg(0x11, 0b00000110)
+        self.write_reg_byte(0x10, 0b00000110)
+        self.write_reg_byte(0x11, 0b00000110)
 
         # TODO ADD SECTION THAT DEFINES UNIT CONVERSION CONSTANTS BASED ON CONFIG 
         # SEE p.12 
 
-        print("CONFIG COMPLETE") 
-        
+        print(f"CONFIG COMPLETE, TEMP READING: {self.get_temp()}") 
 
-        
+    def get_temp(self): 
+        """
+        Reads the current temperature according to the 0x20 and 0x21 registers. 
+        Result in degrees C.  
+        """
+        # The info is 2 bytes. Each stored in 0x20 and 0x21 respectively, with 0x20 being the lower one. 
+        # read_word_data reads 2 bytes and treats the first one as the lower one, so no further rearranging needed. 
+        raw = self.bus.read_word_data(self.TAD, 0x20) 
+        return unsigned_to_signed(raw) / 256 + 25 # Units based on p.16 of user manual 
+
+    def get_pitch_roll_yaw_speeds(self)->tuple[int,int,int]: 
+        """
+        Gets the raw angular rate (in mdps) for the:
+        - X (pitch) axis from the 0x22 and 0x23 registers. 
+        - Y (roll)  axis from the 0x24 and 0x25 registers. 
+        - Z (yaw)   axis from the 0x26 and 0x27 registers. 
+
+        **The conversion units used depend on the selected gyro dps bandwidth.** 
+        """
+        pitch = unsigned_to_signed(self.bus.read_word_data(self.TAD, 0x22)) 
+        roll  = unsigned_to_signed(self.bus.read_word_data(self.TAD, 0x24)) 
+        yaw   = unsigned_to_signed(self.bus.read_word_data(self.TAD, 0x26)) 
+        return pitch*self.LSB_TO_MDPS, roll*self.LSB_TO_MDPS, yaw*self.LSB_TO_MDPS
 
 with LSM6DSV320X(False) as imu: 
-    ## Reading temperature OUT_TEMP_L OUT_TEMP_H 
-    raw = imu.read_reg(0x21)<<8|imu.read_reg(0x20) 
-    # Converting unsigned int to signed 
-    raw = raw - 0x10000 if raw & 0x8000 else raw
-    # Temp unit is LSB/C on a scale of 256. 0 LSB at 25C 
-    temperature_c = raw / 256 + 25
-    print(f"{temperature_c=}")
 
-    ## PITCH angular speed OUTX_L_G OUTX_H_G 
-    raw = imu.read_reg(0x23)<<8|imu.read_reg(0x22) 
-    raw = raw - 0x10000 if raw & 0x8000 else raw
-    # NOTE TODO at specifically 500dps, we have 17.5mdps/LSB. ADD config layer that changes internal conversion based on p.12 
-    pitch_v = raw*17.5 
-    print(f"pitch speed: {pitch_v}mdps")
-    # TODO when picking back up, simplify into class functions 
 
+    ## ANGULAR SPEEDS 
+    print(imu.get_pitch_roll_yaw_speeds())
